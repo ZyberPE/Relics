60bb4839f212aff03aae4eaca5ce04088d5d72ea
ZyberPE <zyberpe@gmail.com>
2026-03-05 00:01:55 -0500

    Update RelicManager.php

--- src/RelicSystem/RelicManager.php
+++ src/RelicSystem/RelicManager.php
@@ -10,7 +10,7 @@ use pocketmine\nbt\tag\StringTag;
 use pocketmine\item\enchantment\EnchantmentInstance;
 use pocketmine\item\enchantment\VanillaEnchantments;
 
-class RelicManager {
+class RelicManager{
 
     private Main $plugin;
 
@@ -18,37 +18,48 @@ class RelicManager {
         $this->plugin = $plugin;
     }
 
-    public function createRelicItem(string $name){
+    public function createRelic(string $name){
+
         $data = $this->plugin->getConfig()->get("relics")[$name] ?? null;
         if($data === null) return null;
 
         $item = VanillaItems::NETHER_STAR();
+
         $item->setCustomName($data["name"]);
         $item->setLore($data["lore"]);
 
-        $item->getNamedTag()->setTag("relic_name", new StringTag($name));
-        $item->addEnchantment(new EnchantmentInstance(VanillaEnchantments::UNBREAKING(), 1));
+        $item->getNamedTag()->setTag("relic_name",new StringTag($name));
+
+        $item->addEnchantment(
+            new EnchantmentInstance(VanillaEnchantments::UNBREAKING(),1)
+        );
 
         return $item;
     }
 
-    public function activateRelic(Player $player, string $name): void {
+    public function activateRelic(Player $player,string $name): void{
+
         $data = $this->plugin->getConfig()->get("relics")[$name];
+
         $rewards = $data["rewards"];
 
-        $totalWeight = 0;
-        foreach($rewards as $reward){
-            $totalWeight += $reward["chance"];
+        $total = 0;
+
+        foreach($rewards as $r){
+            $total += $r["chance"];
         }
 
-        $rand = mt_rand(1, $totalWeight);
+        $rand = mt_rand(1,$total);
         $current = 0;
 
         foreach($rewards as $reward){
+
             $current += $reward["chance"];
+
             if($rand <= $current){
 
-                $cmd = str_replace("{player}", $player->getName(), $reward["command"]);
+                $cmd = str_replace("{player}",$player->getName(),$reward["command"]);
+
                 $this->plugin->getServer()->dispatchCommand(
                     $this->plugin->getServer()->getConsoleSender(),
                     $cmd
