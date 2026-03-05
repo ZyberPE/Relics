2f732b91a5d99cfc23cd214807730dd6d650aca1
ZyberPE <zyberpe@gmail.com>
2026-03-05 00:02:23 -0500

    Update Main.php

--- src/RelicSystem/Main.php
+++ src/RelicSystem/Main.php
@@ -6,79 +6,106 @@ namespace RelicSystem;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\player\Player;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\block\Chest;
-use pocketmine\player\Player;
-use pocketmine\utils\Config;
 use pocketmine\nbt\tag\StringTag;
-use pocketmine\world\Position;
 
-class Main extends PluginBase implements Listener {
+use RelicSystem\command\RelicCommand;
+use RelicSystem\command\RelicsCommand;
 
-    private RelicManager $manager;
+class Main extends PluginBase implements Listener{
+
+    private RelicManager $relicManager;
     private Config $messages;
     private Config $chests;
+
     private array $creating = [];
 
-    protected function onEnable() : void {
+    protected function onEnable(): void{
+
         $this->saveDefaultConfig();
         $this->saveResource("messages.yml");
-        $this->messages = new Config($this->getDataFolder() . "messages.yml", Config::YAML);
-        $this->chests = new Config($this->getDataFolder() . "chests.yml", Config::YAML);
+        $this->saveResource("chests.yml");
 
-        $this->manager = new RelicManager($this);
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->messages = new Config($this->getDataFolder()."messages.yml");
+        $this->chests = new Config($this->getDataFolder()."chests.yml");
+
+        $this->relicManager = new RelicManager($this);
+
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+
+        $this->getServer()->getCommandMap()->register("relic",new RelicCommand($this));
+        $this->getServer()->getCommandMap()->register("relics",new RelicsCommand($this));
     }
 
-    public function getRelicManager(): RelicManager {
-        return $this->manager;
+    public function getRelicManager(): RelicManager{
+        return $this->relicManager;
     }
 
-    public function getMessages(): Config {
+    public function getMessages(): Config{
         return $this->messages;
     }
 
-    public function getChestConfig(): Config {
+    public function getChestConfig(): Config{
         return $this->chests;
     }
 
-    public function setCreating(Player $player, string $relic): void {
+    public function setCreating(Player $player,string $relic): void{
         $this->creating[$player->getName()] = $relic;
     }
 
-    public function onInteract(PlayerInteractEvent $event): void {
+    public function getCreating(Player $player): ?string{
+        return $this->creating[$player->getName()] ?? null;
+    }
+
+    public function clearCreating(Player $player): void{
+        unset($this->creating[$player->getName()]);
+    }
+
+    public function onInteract(PlayerInteractEvent $event): void{
+
         $player = $event->getPlayer();
         $block = $event->getBlock();
 
         if(!$block instanceof Chest) return;
 
-        $posKey = $block->getPosition()->getWorld()->getFolderName() . ":" .
-            $block->getPosition()->getFloorX() . ":" .
-            $block->getPosition()->getFloorY() . ":" .
-            $block->getPosition()->getFloorZ();
+        $pos = $block->getPosition();
 
-        // Creating chest
-        if(isset($this->creating[$player->getName()])){
-            $relic = $this->creating[$player->getName()];
-            $this->chests->set($posKey, $relic);
+        $key =
+            $pos->getWorld()->getFolderName().":".
+            $pos->getFloorX().":".
+            $pos->getFloorY().":".
+            $pos->getFloorZ();
+
+        // create relic chest
+        if(($relic = $this->getCreating($player)) !== null){
+
+            $this->chests->set($key,$relic);
             $this->chests->save();
-            unset($this->creating[$player->getName()]);
+
+            $this->clearCreating($player);
+
             $player->sendMessage($this->messages->get("relic-created"));
             return;
         }
 
-        // Using relic
-        if(!$this->chests->exists($posKey)) return;
+        // using relic
+        if(!$this->chests->exists($key)) return;
 
         $item = $player->getInventory()->getItemInHand();
         $tag = $item->getNamedTag()->getTag("relic_name");
+
         if(!$tag instanceof StringTag) return;
 
-        $relicName = $tag->getValue();
-        if($this->chests->get($posKey) !== $relicName) return;
+        $relic = $tag->getValue();
 
-        $this->manager->activateRelic($player, $relicName);
-        $item->setCount($item->getCount() - 1);
+        if($this->chests->get($key) !== $relic) return;
+
+        $this->relicManager->activateRelic($player,$relic);
+
+        $item->setCount($item->getCount()-1);
         $player->getInventory()->setItemInHand($item);
     }
 }
